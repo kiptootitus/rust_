667ba6390f0b857236d3cfa9300b71e34844d302
naruto <kiptootitus75@gmail.com>
2025-05-01 20:56:06 +0300

    constants

--- _07constants/src/main.rs
+++ _07constants/src/main.rs
@@ -1,3 +1,11 @@
+#! allow
+// Constants 
+
 fn main() {
     println!("Hello, world!");
+    let a: i32 = 34;
+    println!("The value of variable a is {}", a);
+    // You can declare a constant with type annotation e.g i32, u32
+    const Y: u32 = 10;
+    println!("The value of constant Y is {}", Y);
 }
