cee6d388b6dd1c38eb3575ff64e2839b17a17546
naruto <kiptootitus75@gmail.com>
2025-05-01 21:14:12 +0300

    shadowing

--- _07constants/src/main.rs
+++ _07constants/src/main.rs
@@ -8,4 +8,10 @@ fn main() {
     // You can declare a constant with type annotation e.g i32, u32
     const Y: u32 = 10;
     println!("The value of constant Y is {}", Y);
+    println!("The value of pi is {}", PI);
+
+
 }
+
+
+const PI: f64 = 3.14159265359;
---
+++ _08shadowing/src/main.rs
@@ -0,0 +1,16 @@
+// Shadowing 
+// Shadowing is different from mutability is that mutability uses `mut` which makes the variable reassignable.
+fn main() {
+    // original variable
+    let x = 5;
+    // this variable will overshadow the original variable and it can be overshadow by the other variable declare at the last line of scoper
+    let x = x + 7;
+    let x = x * 2;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
