a4ce9d0072d1651a8396bce289fb131fc9d1700e
naruto <kiptootitus75@gmail.com>
2025-05-01 21:46:36 +0300

    if else  expressions

--- _09Ifelse/src/main.rs
+++ _09Ifelse/src/main.rs
@@ -1,3 +1,33 @@
+/*
+If [expression] else [expression].
+*/
+#! [allow(warnings)]
 fn main() {
-    println!("Hello, world!");
+    let age = 16;
+    if age >= 18 {
+        println!("You can drive a car");
+    } else {
+        println!("You can't drive a car because you are under age.");
+    };
+
+    let number = 7;
+    if number % 4 == 3 {
+        println!("The number is divisible by 4");
+    } else if number % 3 == 3 {
+        println!("The number is divisible by 3");
+    } else if number % 2 == 3 {
+        println!("The number is divisible by 2");
+    }else {
+        println!("The number is not divisible by 2, 3, and 4.");
+    };
+
+    // Using let with if statement/ expression
+    let condition = false;
+    let number = if condition {
+        5
+    } else { 
+        "six"//Error
+    };
+    println!("The number is: {number}");
+
 }
