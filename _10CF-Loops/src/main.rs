dc7c82de961f86a8519d4d18900df8cb9fe35313
naruto <kiptootitus75@gmail.com>
2025-05-01 22:25:59 +0300

    for loops

--- _10CF-Loops/src/main.rs
+++ _10CF-Loops/src/main.rs
@@ -1,3 +1,51 @@
+/*
+Loops;
+    1. 
+*/
 fn main() {
-    println!("Hello, world!");
+    let mut number = 0;
+    let result = loop {
+        number += 1;
+        if number == 24 {
+            break number - 300;
+        }
+    };
+    println!("The result of the loop is {result}");
+
+    // Nested Loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1; // this one will make to 9 so it will stop and count another one 
+        }
+
+        count += 1;
+    };
+    println!("End count = {count}");
+
+
+    // While loop. This conditions runs and until a certain condition is met
+    let mut days = 0;
+    while days <= 10{
+        println!("The number of days is {}", days);
+        days += 1;
+    };
+    println!("LIFTOFF!!!");
+
+    // for loop. it is used to iterable over a list or array
+    let num = [1,2,3,4,5,6,7,8,9];
+
+    for number in num {
+        println!("Itering over num array. Number {number}");
+    }
 }
