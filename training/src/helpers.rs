393f3e00fd4326d7594fb418382fbf378279f6b4
naruto <kiptootitus75@gmail.com>
2025-05-03 22:36:37 +0300

    datavase connection

--- training/src/helpers.rs
+++ training/src/helpers.rs
@@ -1,5 +1,11 @@
 pub mod helpersmod {
-  pub fn get_full_name(first: &str, middle: &str, last: &str) -> String {
-      format!("{} {} {}", first.trim(), middle.trim(), last.trim())
-  }
+    pub fn get_full_name(first: &str, middle: &str, last: &str) -> String {
+        format!("{} {} {}", first.trim(), middle.trim(), last.trim())
+    }
+}
+
+pub mod privatefns {
+    pub fn get_age(age: u32) -> u32 {
+        return age + 5;
+    }
 }
--- training/src/main.rs
+++ training/src/main.rs
@@ -1,8 +1,7 @@
-#! [allow(warnings)]
+#![allow(warnings)]
 
-pub mod helpers;
 mod database;
-
+pub mod helpers;
 #[tokio::main]
 async fn main() {
     let my_name = helpers::helpersmod::get_full_name("Titus", " Kiptoo", " Kibet");
@@ -19,5 +18,6 @@ async fn main() {
             eprintln!("❌ Failed to fetch names: {e}");
         }
     }
-
+    let age = helpers::privatefns::get_age(23);
+    println!("The new age is {0}", age);
 }
